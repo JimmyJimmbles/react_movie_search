f6ccfc08874e49fa5391f785195beab7fb5edc9f
Jay Jones <jaypjonesofficial@gmail.com>
2018-10-02 22:53:44 -0500

    Adding responsive grid classes in scss

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,23 +2,26 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    const headerStyles = {
+    const headerParent = {
       backgroundColor: "#2d2d2d",
-      width: "100%"
+      width: "100%",
+      color: "#fff"
     };
     return (
       <div>
-        <header id="Header" className="main-header header" style={headerStyles}>
-          <div className="row">
-            <div className="col-12 col-sm-6">
-              <img
-                className="img-responsive header-logo"
-                src="green-logo.svg"
-                alt="React Movie Search Logo"
-              />
-            </div>
-            <div className="col-12 col-sm-6">
-              <h1 className="header-title">React Movie </h1>
+        <header id="Header" className="main-header header" style={headerParent}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="grid-2">
+                <img
+                  className="img-responsive header-logo"
+                  src="green-logo.svg"
+                  alt="React Movie Search Logo"
+                />
+              </div>
+              <div className="grid-8">
+                <h1 className="header-title">React Movie Search</h1>
+              </div>
             </div>
           </div>
         </header>
