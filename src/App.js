f286e75dbcd22a94949e414d013d3f482ed195f9
Jay Jones <jaypjonesofficial@gmail.com>
2018-10-02 22:16:57 -0500

    adding mixins and variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.scss";
 import "./styles/_images.scss";
+import "./styles/_grid.scss";
 import Header from "./Components/Header";
 
 class App extends Component {
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,15 +9,17 @@ class Header extends Component {
     return (
       <div>
         <header id="Header" className="main-header header" style={headerStyles}>
-          <div className="col-12 col-sm-4">
-            <img
-              className="img-responsive header-logo"
-              src="green-logo.svg"
-              alt="React Movie Search Logo"
-            />
-          </div>
-          <div className="col-12 col-sm-8">
-            <h1 className="header-title">React Movie </h1>
+          <div className="row">
+            <div className="col-12 col-sm-6">
+              <img
+                className="img-responsive header-logo"
+                src="green-logo.svg"
+                alt="React Movie Search Logo"
+              />
+            </div>
+            <div className="col-12 col-sm-6">
+              <h1 className="header-title">React Movie </h1>
+            </div>
           </div>
         </header>
       </div>
